c7a82dcfea54f78e552dfd7d38b3a100a8a3c10f
EgorWK4 <47928799+EgorWK4@users.noreply.github.com>
2020-01-27 06:38:51 +0300

    Add files via upload

---
+++ template_stack.cpp
@@ -0,0 +1,60 @@
+﻿
+
+#include <vector>
+#include <list>
+#include <iostream>
+#include <string>
+
+template <class T, class Container = std::vector<T>>
+struct Stack {
+
+private:
+    Container cont;
+
+public:
+    using container_type = Container;
+    using value_type = typename Container::value_type;
+    using reference = typename Container::reference;
+    using size_type = typename Container::size_type;
+    using const_reference = typename Container::const_reference;
+    
+public:
+    void push(const value_type& x)
+    {
+        cont.push_back(x);
+    }
+    reference top() {
+        return cont.back();
+    }
+
+    bool empty() const {
+        return cont.empty();
+    }
+
+    size_type size() const {
+        cont.size();
+    }
+
+    void swap(Stack& other) noexcept {
+        using std::swap;
+        swap(cont, other.cont);
+    }
+
+    void pop() noexcept {
+      
+        cont.pop();
+    }
+
+};
+int main()
+{
+    Stack<int> a;
+    a.push(10);
+    a.push(20);
+    Stack <std::string> b;
+    b.push("tttt");
+    std::cout << a.top();
+    std::cout << b.top();
+    
+
+}
